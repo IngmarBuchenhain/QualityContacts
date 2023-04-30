42282508d58c4e616a2805b72566b366efa1c4f5
IngmarBuchenhain <71217928+IngmarBuchenhain@users.noreply.github.com>
2023-04-30 23:11:39 +0200

    Start implementation of ContactParser

--- App/QualityContacts.Services/Services/ContactParser.cs
+++ App/QualityContacts.Services/Services/ContactParser.cs
@@ -4,13 +4,45 @@ using QualityContacts.Services.Models;
 
 namespace QualityContacts.Services
 {
+    /// <summary>
+    /// <inheritdoc cref="IContactParser"/><br/>
+    /// Implementation of <see cref="IContactParser"/>.
+    /// </summary>
     public class ContactParser : IContactParser
     {
 
+        #region Public Methods
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns><inheritdoc/></returns>
         public IContact ParseContactInput(string input)
         {
+
+
+            var contactParts = SplitStringToPossibleContactParts(input);
+
+
             return new Contact();
         }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private string[] SplitStringToPossibleContactParts(string input, char separator)
+        {
+            var parts = input.Split(separator);
+
+            
+
+            return parts;
+        }
+
+
+
+        #endregion Private Methods
     }
 }
