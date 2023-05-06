332c4d244d9942dff41ca4d1562a3923bbc6e136
IngmarBuchenhain <71217928+IngmarBuchenhain@users.noreply.github.com>
2023-05-06 12:02:51 +0200

    Finish implementation of parser for salutation, academic title, first, middle and last name

--- App/QualityContacts.Services/Services/ContactParser.cs
+++ App/QualityContacts.Services/Services/ContactParser.cs
@@ -11,8 +11,23 @@ namespace QualityContacts.Services
     /// </summary>
     public class ContactParser : IContactParser
     {
+        #region Members and Constructors
+
+
+        public ContactParser()
+        {
+            _registeredSalutations = new ContactRepository().GetRegisteredSalutations();
+        }
+
+        private IEnumerable<string> _registeredSalutations;
+        private ContactRepository _contactRepository = new ContactRepository();
+
+        
+
+        #endregion Members and Constructors
 
         #region Public Methods
+
 
         /// <summary>
         /// <inheritdoc/>
@@ -29,34 +44,143 @@ namespace QualityContacts.Services
 
             // Extract words from free input string. Assume words are separated by spaces.
             string[] contactParts = SplitIntoWords(contactCandidate, ' ');
-
-            // Based on number of words in input, choose further parsing method.
-
+        
             // Case the input string only contained spaces.
             if (contactParts.Length == 0) return newContact;
 
-            // Special case with only one given word: We assume the given word is the last name!
-            if (contactParts.Length == 1)
+            // Iterate through all words, beginning with first and apply priorised rules
+            int numberOfWords = contactParts.Length;
+
+            for ( int wordIndex = 0; wordIndex < numberOfWords; wordIndex++ )
             {
-                newContact.LastName = contactParts[0];
-                return newContact;
-            } else
-            {
-                // In case of more then one word, check whether one word ends with ',', in this case this is the last name, otherwise assume the last word is the last name.
-                string possibleRevertedLastName;
-                if(TryFindRevertedLastName(out possibleRevertedLastName, contactParts))
+                // Get current word
+                string currentWord = contactParts[wordIndex];
+
+                // RULE: It it is the last word, it is the last name
+                if (wordIndex == numberOfWords - 1)
                 {
-                    newContact.LastName = possibleRevertedLastName;
+                    // Append to last name, add space if needed
+                    bool alreadyLastNameExisting = newContact.LastName.Length > 0;
+                    if (alreadyLastNameExisting)
+                    {
+                        newContact.LastName += " " + currentWord;
+                    }
+                    else
+                    {
+                        newContact.LastName += currentWord;
+                    }
+                } else if (wordIndex == 0)
+                {
+                    // RULE: The first word may be a salutation.
+                    // Check this first
+ 
+
+
+                    foreach(string salutation in _registeredSalutations)
+                    {
+                        if (currentWord.ToLower().Equals(salutation.ToLower()))
+                        {
+                            newContact.Salutation = currentWord;
+       
+                            break;
+                        }
+                    }
+
+                    // If a salutation could be extracted, determine the gender.
+                    if (!String.IsNullOrEmpty(newContact.Salutation))
+                    {
+                        newContact.Gender = new ContactRepository().GetGender(newContact.Salutation);
+                        continue;
+                    }
+
+                    // RULE: If no salutation check for academic title
+                    // Check this second
+
+                    foreach(string academicTitle in _contactRepository.GetRegisteredAcademicTitles())
+                    {
+                        if (currentWord.ToLower().Equals(academicTitle.ToLower()))
+                        {
+                            newContact.Titles = academicTitle;
+
+                            break;
+                        }
+                    }
+
+                    if (!String.IsNullOrEmpty(newContact.Titles)) continue;
+
+                    // RULE: If no academic title check for noble title
+                    // Check this third
+
+                    // TODO: Implement
+
+                    // RULE: If no applies, it should be a first name.
+                    newContact.FirstName = currentWord;
                 } else
                 {
-                    newContact.LastName = contactParts[contactParts.Length - 1];
+                    // RULE: Middle words may be titles or first/middle names
+
+                    // Check for academic titles first
+                    bool foundAcademicTitle = false;
+                    foreach (string academicTitle in _contactRepository.GetRegisteredAcademicTitles())
+                    {
+                        if (currentWord.ToLower().Equals(academicTitle.ToLower()))
+                        {
+                            foundAcademicTitle = true;
+                            if (!String.IsNullOrEmpty(newContact.Titles))
+                            {
+                                newContact.Titles += " " + academicTitle;
+                            }
+                            else
+                            {
+                                newContact.Titles = academicTitle;
+                            }
+                            
+
+                            break;
+                        }
+                    }
+                    if (foundAcademicTitle) continue;
+
+                    // If no academic title, check for noble titles
+
+                    // If no applies, it should be a first or middle name.
+                    if (!String.IsNullOrEmpty(newContact.FirstName))
+                    {
+                        newContact.FirstName += " " + currentWord;
+                    } else
+                    {
+                        newContact.FirstName = currentWord;
+                    }
                 }
 
-                // Get all titles
-                
 
-                return newContact;
+                
             }
+
+            return newContact;
+
+            //// Special case with only one given word: We assume the given word is the last name!
+            //if (contactParts.Length == 1)
+            //{
+            //    newContact.LastName = contactParts[0];
+            //    return newContact;
+            //} else
+            //{
+            //    // In case of more then one word, check whether one word ends with ',', in this case this is the last name, otherwise assume the last word is the last name.
+            //    string possibleRevertedLastName;
+            //    if(TryFindRevertedLastName(out possibleRevertedLastName, contactParts))
+            //    {
+            //        newContact.LastName = possibleRevertedLastName;
+            //    } else
+            //    {
+            //        newContact.LastName = contactParts[contactParts.Length - 1];
+            //    }
+
+            //    // Get all titles
+
+
+            //    return newContact;
+            //}
         }
 
         #endregion Public Methods
--- App/QualityContacts.Services/Services/ContactRepository.cs
+++ App/QualityContacts.Services/Services/ContactRepository.cs
@@ -7,6 +7,46 @@ namespace QualityContacts.Services
 {
     public class ContactRepository : IContactRepository
     {
+
+        public ContactRepository() {
+            AddToSalutationGenders("Herr", MALE_GENDER);
+            AddToSalutationGenders("Herrn", MALE_GENDER);
+            AddToSalutationGenders("Mr", MALE_GENDER);
+            AddToSalutationGenders("Señor", MALE_GENDER);
+            AddToSalutationGenders("M", MALE_GENDER);
+
+            AddToSalutationGenders("Frau", FEMALE_GENDER);
+            AddToSalutationGenders("Mrs", FEMALE_GENDER);
+            AddToSalutationGenders("Ms", FEMALE_GENDER);
+            AddToSalutationGenders("Signora", FEMALE_GENDER);
+            AddToSalutationGenders("Sig.", FEMALE_GENDER);
+            AddToSalutationGenders("Mme", FEMALE_GENDER);
+            AddToSalutationGenders("Señora", FEMALE_GENDER);
+
+            // Default academic titles
+            REGISTERED_ACADEMIC_TITLES.Add("Dr.");
+            REGISTERED_ACADEMIC_TITLES.Add("Professor");
+            REGISTERED_ACADEMIC_TITLES.Add("Professorin");
+            REGISTERED_ACADEMIC_TITLES.Add("Prof.");
+            REGISTERED_ACADEMIC_TITLES.Add("Dr.-Ing.");
+            REGISTERED_ACADEMIC_TITLES.Add("h.c.");
+            REGISTERED_ACADEMIC_TITLES.Add("mult.");
+            REGISTERED_ACADEMIC_TITLES.Add("rer.");
+            REGISTERED_ACADEMIC_TITLES.Add("nat.");
+            REGISTERED_ACADEMIC_TITLES.Add("Dipl.");
+            REGISTERED_ACADEMIC_TITLES.Add("Dipl-Ing.");
+            REGISTERED_ACADEMIC_TITLES.Add("Ing.");
+            REGISTERED_ACADEMIC_TITLES.Add("B.S.");
+            REGISTERED_ACADEMIC_TITLES.Add("M.S.");
+        }
+
+        private void AddToSalutationGenders(string key, string value)
+        {
+            if (REGISTERED_SALUTATION_TO_GENDER.ContainsKey(key)) return;
+
+            REGISTERED_SALUTATION_TO_GENDER.Add(key, value);
+        }
+
         public IContact GetNewContact()
         {
             return new Contact();
@@ -56,9 +96,30 @@ namespace QualityContacts.Services
             return REGISTERED_NOBLE_PRÄ_SUFFIXES;
         }
 
+        internal string GetGender(string salutation)
+        {
+            if (REGISTERED_SALUTATION_TO_GENDER.ContainsKey(salutation))
+            {
+                return REGISTERED_SALUTATION_TO_GENDER[salutation];
+            }
+            return String.Empty;
+        }
+
+        internal IEnumerable<string> GetRegisteredAcademicTitles()
+        {
+            return REGISTERED_ACADEMIC_TITLES;
+        }
+
         private HashSet<string> _titles = new HashSet<string>();
 
         private ObservableCollection<IContact> _contacts = new ObservableCollection<IContact>();
+
+        private static readonly Dictionary<string, string> REGISTERED_SALUTATION_TO_GENDER = new Dictionary<string, string>();
+
+        private const string MALE_GENDER = "männlich";
+        private const string FEMALE_GENDER = "weiblich";
+        private const string DIVERSE_GENDER = "divers";
+        private const string EMPTY_GENDER = "ohne";
 
         private static readonly HashSet<string> REGISTERED_GENDERS = new HashSet<string> { "männlich", "weiblich", "divers", "ohne" };
 
@@ -67,5 +128,7 @@ namespace QualityContacts.Services
         private static readonly HashSet<string> REGISTERED_NOBLE_TITLES = new HashSet<string> { "Prinz", "Prinzessin", "Sir", "Dame", "Freiherrin", "Freiherr", "Baron", "Baronesse", "Ritter", "Graf", "Gräfin", "Fürst", "Fürstin", "Markgraf", "Pfalzgraf", "Landgraf", "Herzog", "Herzogin", "Kurfürst", "Großherzog", "Erzherzog", "König", "Königin" };
 
         private static readonly HashSet<string> REGISTERED_NOBLE_PRÄ_SUFFIXES = new HashSet<string> { "von", "vom", "van", "de", "zu", "zur" };
+
+        private static readonly HashSet<string> REGISTERED_ACADEMIC_TITLES = new HashSet<string> { };
     }
 }
