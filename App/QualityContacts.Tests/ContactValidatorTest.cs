4f6f98abd05a225d8669342d39616f1acc2a52ef
FreudenbergerJ <i20013@hb.dhbw-stuttgart.de>
2023-05-07 18:30:42 +0200

    Add ContactValidatorTest

---
+++ App/QualityContacts.Tests/ContactValidatorTest.cs
@@ -0,0 +1,87 @@
+using QualityContacts.ServiceInterfaces.ErrorHandling;
+using QualityContacts.ServiceInterfaces.Models;
+using QualityContacts.Services;
+using QualityContacts.Services.ErrorHandling;
+using QualityContacts.Services.Models;
+
+namespace QualityContacts.Tests
+{
+    public class ContactValidatorTest
+    {
+
+        [Fact]
+        public void HasWarningsAtSemikolonInput()
+        {
+            ContactValidator contactValidator = new ContactValidator(new ContactRepository());
+
+            IValidationResult result = contactValidator.Validate("Mustermann; Max");
+
+            Assert.Equal(true, result.HasWarnings);
+        }
+
+        [Fact]
+        public void HasWarningsAccordingToMissingGender()
+        {
+            Contact contact = new Contact();
+            contact.FirstAndMiddleName = "Max";
+            contact.LastName = "Mustermann";
+            contact.Gender = "";
+            contact.Salutation = "Herr";
+            contact.Titles = "Dr.";
+
+            ContactValidator contactValidator = new ContactValidator(new ContactRepository());
+
+            IValidationResult result = contactValidator.Validate(contact);
+
+            Assert.True(result.HasWarnings);
+        }
+
+        [Fact]
+        public void HasWarningsAccordingToMissingSalutation()
+        {
+            Contact contact = new Contact();
+            contact.FirstAndMiddleName = "Max";
+            contact.LastName = "Mustermann";
+            contact.Gender = "männlich";
+            contact.Titles = "Dr.";
+
+            ContactValidator contactValidator = new ContactValidator(new ContactRepository());
+
+            IValidationResult result = contactValidator.Validate(contact);
+
+            Assert.True(result.HasWarnings);
+        }
+
+        [Fact]
+        public void HasWarningsAccordingToMissingFirstName()
+        {
+            Contact contact = new Contact();
+            contact.LastName = "Mustermann";
+            contact.Gender = "männlich";
+            contact.Salutation = "Herr";
+            contact.Titles = "Dr.";
+
+            ContactValidator contactValidator = new ContactValidator(new ContactRepository());
+
+            IValidationResult result = contactValidator.Validate(contact);
+
+            Assert.True(result.HasWarnings);
+        }
+
+        [Fact]
+        public void HasWarningsAccordingToMissingLastName()
+        {
+            Contact contact = new Contact();
+            contact.FirstAndMiddleName = "Max";
+            contact.Gender = "männlich";
+            contact.Salutation = "Herr";
+            contact.Titles = "Dr.";
+
+            ContactValidator contactValidator = new ContactValidator(new ContactRepository());
+
+            IValidationResult result = contactValidator.Validate(contact);
+
+            Assert.True(result.HasWarnings);
+        }
+    }
+}
